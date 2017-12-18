326530f3db868942f20dea61d2d2e93401658de3
Moritz <moritzahorner@gmail.com>
2017-12-18 13:29:26 +0100

    test

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
 public class Controller {
+
+
+    @FXML
+    Pane secPane;
+    public void loadFxml (ActionEvent event) throws IOException {
+        Pane newLoadedPane =        FXMLLoader.load(getClass().getResource("test.fxml"));
+        secPane.getChildren().add(newLoadedPane);
+    }
+
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("suchen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("MainPage");
         primaryStage.setScene(new Scene(root, 600, 450));
         primaryStage.show();
