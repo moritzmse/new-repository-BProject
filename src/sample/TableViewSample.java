52af09c3833f8743c5354b58688c655b8940dcb9
Moritz <moritzahorner@gmail.com>
2017-12-18 15:40:10 +0100

    tableviewsample

--- src/sample/TableViewSample.java
+++ src/sample/TableViewSample.java
@@ -73,40 +73,4 @@ public class TableViewSample extends Application {
         stage.show();
     }
 
-    public static class Person {
-
-        private final SimpleStringProperty firstName;
-        private final SimpleStringProperty lastName;
-        private final SimpleStringProperty email;
-
-        private Person(String fName, String lName, String email) {
-            this.firstName = new SimpleStringProperty(fName);
-            this.lastName = new SimpleStringProperty(lName);
-            this.email = new SimpleStringProperty(email);
-        }
-
-        public String getFirstName() {
-            return firstName.get();
-        }
-
-        public void setFirstName(String fName) {
-            firstName.set(fName);
-        }
-
-        public String getLastName() {
-            return lastName.get();
-        }
-
-        public void setLastName(String fName) {
-            lastName.set(fName);
-        }
-
-        public String getEmail() {
-            return email.get();
-        }
-
-        public void setEmail(String fName) {
-            email.set(fName);
-        }
-    }
 }
