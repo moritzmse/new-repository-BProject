efcf33326d5cb00a05ff81adb7b31458752e9c9c
Moritz <moritzahorner@gmail.com>
2018-02-28 16:24:20 +0100

    -

--- src/main/Search.java
+++ src/main/Search.java
@@ -418,5 +418,6 @@ public class Search {
     //Auf das Löschen von Tabs reagieren!!!!!!!!!!!!!
     //Bei "Suchen" wieder in Ergebnis-Tab springen
     //get tabs methode überarbeiten
+    //wenn alle Tabs geschlossen sind: Max, Min, etc Berechnungen ausblenden
 
 }
